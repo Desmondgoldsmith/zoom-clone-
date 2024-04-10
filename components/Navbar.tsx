080c4e956da7a1f0f9342f87ccc1ae7c3ee8afdc
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-04-10 02:05:54 +0000

    feat: working on the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className = "bg-dark-1  fixed w-full z-50 flex-between px-6 py-4 lg:px-10 ">
+      <Link href = '/'>
+         <Image 
+           src = "/icon/logo.svg"
+           alt = "zoom logo"
+           height = {32}
+           width = {32}
+           className = "max-sm:size-10"
+         />
+         <p className = "text-[26px] font-extrabold text-white max-sm:hidden">zoom</p>
+      </Link>
+
+      <div className = "flex-between gap-5">
+        // clerk comes in here
+      </div>
+    </nav>
   )
 }
 
